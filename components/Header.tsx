ad9a1f69c0ef2fb84befae1e26fe4257b4f340d1
Mayank <mayanksingh8181335335@gmail.com>
2024-12-02 06:52:54 +0530

    added some ui components and updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="flex justify-between items-center p-4 bg-white shadow-md">
+      <div className="flex items-center justify-center">
+        <Image src="/images/logo_1.png" alt="Prime Electronics" height={30} width={100}/>
+      </div>
+
+<div className="ml-auto flex gap-2">
+
+<div>
+    Search
+    {/* TODO Search */}
+</div>
+
+      <nav className="ml-auto">
+        navigation
+        {/* TODO */}
+        {/* Cart */}
+        {/* Wishlist */}
+      </nav>
+</div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
