8f2ce2e02327db81c4e386ce3cca9e42275820c5
Mayank <mayanksingh8181335335@gmail.com>
2024-12-10 17:56:35 +0530

    made some updatews

--- components/Brands.tsx
+++ components/Brands.tsx
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from "react";
 
-// import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 import { db } from "@/firebase/firebaseConfig";
-import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import Image from "next/image";
 
-
-type brand={
+type brand = {
   id: string;
-  name:string;
-}
-
+  name: string;
+};
 
 function Brands() {
   const [featuredBrands, setFeaturedBrands] = useState<brand[]>([]);
@@ -30,7 +41,6 @@ function Brands() {
 
           const brandIdsArray = [];
 
-
           //  field id is string
           for (let i = 0; i < 8; i++) {
             if (data[i.toString()]) {
@@ -38,37 +48,25 @@ function Brands() {
             }
           }
 
+          try {
+            const ref = collection(db, "brands");
 
+            const brandsQuery = query(
+              ref,
+              where("__name__", "in", brandIdsArray)
+            );
 
-          try{
+            const querySnapshot = await getDocs(brandsQuery);
 
-            const ref=collection(db,'brands');
-
-
-            const brandsQuery=query(ref,where("__name__","in",brandIdsArray));
-
-            const querySnapshot=await getDocs(brandsQuery);
-
-            const brands:brand[]=querySnapshot.docs.map(doc =>({
-              id:doc.id,name:doc.data().name
-            }))
+            const brands: brand[] = querySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              name: doc.data().name,
+            }));
 
             setFeaturedBrands(brands);
-
-
-
-
-
-
-          }catch(err)
-          {
-            console.error('some error occurred', err);
+          } catch (err) {
+            console.error("some error occurred", err);
           }
-
-
-
-
-
 
           // setFeaturedBrands(brandIdsArray);
         } else {
@@ -87,9 +85,20 @@ function Brands() {
     return <div>loading...</div>;
   }
 
-  return <div className="border-2 border-black rounded p-1">
-    {featuredBrands.map((b) =><p key={b.id}>{b.name}</p>)}
-  </div>;
+  return (
+    <div className="border-2 border-black rounded p-1 flex flex-row">
+      {featuredBrands.map((b) => (
+        <Card key={b.id}>
+          <CardHeader>
+            <CardTitle>{b.name}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Image src="https://picsum.photos/200" alt={b.name} width={160} height={160} layout="intrinsic"/>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
 }
 
 export default Brands;
