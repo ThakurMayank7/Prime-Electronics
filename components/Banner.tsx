5125490d7e8cc9446794bda5b55de3b54e24850f
Mayank <mayanksingh8181335335@gmail.com>
2024-12-11 17:34:04 +0530

    added custom made carousel for banners

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,16 +1,10 @@
 import React from "react";
+import Carousel from "./Carousel";
 
 function Banner() {
   return (
     <div className="border-2 border-black rounded p-1">
-      <div className="bg-cyan-200 rounded p-1 flex flex-col items-center">
-        <span className="text-2xl font-semibold">Banner</span>
-        <span className="text-2xl font-semibold">Banner</span>
-        <span className="text-2xl font-semibold">Banner</span>
-        <span className="text-2xl font-semibold">Banner</span>
-        <span className="text-2xl font-semibold">Banner</span>
-        <span className="text-2xl font-semibold">Banner</span>
-      </div>
+      <Carousel/>
     </div>
   );
 }
