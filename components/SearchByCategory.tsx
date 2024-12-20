b7e772931bd76aa3af0925d96e2df68b2c35c588
Mayank <mayanksingh8181335335@gmail.com>
2024-12-20 23:53:51 +0530

    made some changes

--- components/SearchByCategory.tsx
+++ components/SearchByCategory.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
 
 function SearchByCategory() {
   return (
-    <div className="border-2 border-black rounded p-1">
-        SearchByCategory
-        </div>
-  )
+    <div className="border-2 border-black rounded p-1">SearchByCategory</div>
+  );
 }
 
-export default SearchByCategory
+export default SearchByCategory;
