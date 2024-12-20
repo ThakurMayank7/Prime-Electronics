065965f285061b451b518822a22f7855c78bbd80
Mayank <mayanksingh8181335335@gmail.com>
2024-12-20 23:54:31 +0530

    made some changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import MobSidebar from "./MobSidebar";
 import { Filter } from "lucide-react";
 import { Separator } from "./ui/separator";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useAuth } from "@/hooks/useAuth";
 
 import {
@@ -15,7 +15,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 import ShineBorder from "./ui/shine-border";
 import { signOutUser } from "@/firebase/auth";
 
@@ -29,32 +29,22 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
-
-
-
-
+} from "@/components/ui/alert-dialog";
 
 function Header() {
   // const [opened,setOpened]=useState();
 
   // const headerSidebarAction=()=>{};
 
-  const {user,loading}=useAuth();
-  
-  
+  const { user, loading } = useAuth();
 
-  if(loading)
-  {
+  if (loading) {
     return null;
-
   }
 
-  const signOutHandler=async ()=>{
+  const signOutHandler = async () => {
     signOutUser();
-
   };
-  
 
   return (
     <header className="bg-pallette1 p-4 flex flex-row items-center">
@@ -89,91 +79,81 @@ function Header() {
         </div>
       </div>
 
-      <div className={`ml-auto text-white flex gap-1 mr-2 p-1 bg-pallette3 rounded-full hover:border-2`}>
-<DropdownMenu 
-// onOpenChange={()=>console.log("dropdown")}
->
-  <DropdownMenuTrigger>
-    
-      <Avatar className="rounded-full">
-  <AvatarImage className="rounded-full" src={user?.photoURL || "https://github.com/shadcn.png"} alt={user?.displayName?.charAt(0) ?? "N/A"}/>
-  <AvatarFallback>CN</AvatarFallback>
-</Avatar>
-    
+      <div
+        className={`ml-auto text-white flex gap-1 mr-2 p-1 bg-pallette3 rounded-full hover:border-2`}
+      >
+        <DropdownMenu
+        // onOpenChange={()=>console.log("dropdown")}
+        >
+          <DropdownMenuTrigger>
+            <Avatar className="rounded-full">
+              <AvatarImage
+                className="rounded-full"
+                src={user?.photoURL || "https://github.com/shadcn.png"}
+                alt={user?.displayName?.charAt(0) ?? "N/A"}
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
 
-  </DropdownMenuTrigger>
-
-
-  <DropdownMenuContent>
-  <ShineBorder>
-    <DropdownMenuLabel className="">
-      <span className="text-xl flex justify-center">
-
-      {user?.displayName}
-      </span>
-      <span className="font-thin">
-        {user?.email}
-      </span>
-
-      </DropdownMenuLabel>
-    <DropdownMenuSeparator />
-    <DropdownMenuItem className="hover:cursor-pointer">
-        <Link href="/profile">
-        <span className="text-lg">
-        Profile
-        </span>
-        </Link>
-    </DropdownMenuItem>
-    <DropdownMenuItem className="hover:cursor-pointer">
-      <Link href="/orders">
-      <span className="text-lg">Orders</span>
-      </Link>
-    </DropdownMenuItem>
-    <DropdownMenuItem className="hover:cursor-pointer">
-      <Link href="/wishlist">
-      <span className="text-lg">Wishlist</span>
-
-      </Link>
-    </DropdownMenuItem>
-    <DropdownMenuItem className="hover:cursor-pointer">
-      <Link href="/cart">
-      <span className="text-lg">Cart</span>
-      </Link>
-
-    </DropdownMenuItem>
-    <DropdownMenuItem onSelect={(e)=>e.preventDefault()}>
-      
-      <AlertDialog>
-      <AlertDialogTrigger asChild>
-        
-
-<button className="bg-red-600 p-2 rounded font-bold text-white hover:text-base">Sign Out</button>
-        
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-             This action will signout this account on this device.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction className="bg-red-600 hover:bg-red-800" onClick={signOutHandler}>
-            
-              Confirm Sign Out
-            
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-
-    </DropdownMenuItem>
-  </ShineBorder>
-  </DropdownMenuContent>
-</DropdownMenu>
-
-
+          <DropdownMenuContent>
+            <ShineBorder>
+              <DropdownMenuLabel className="">
+                <span className="text-xl flex justify-center">
+                  {user?.displayName}
+                </span>
+                <span className="font-thin">{user?.email}</span>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="hover:cursor-pointer">
+                <Link href="/profile">
+                  <span className="text-lg">Profile</span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="hover:cursor-pointer">
+                <Link href="/orders">
+                  <span className="text-lg">Orders</span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="hover:cursor-pointer">
+                <Link href="/wishlist">
+                  <span className="text-lg">Wishlist</span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="hover:cursor-pointer">
+                <Link href="/cart">
+                  <span className="text-lg">Cart</span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <button className="bg-red-600 p-2 rounded font-bold text-white hover:text-base">
+                      Sign Out
+                    </button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                      <AlertDialogDescription>
+                        This action will signout this account on this device.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <AlertDialogAction
+                        className="bg-red-600 hover:bg-red-800"
+                        onClick={signOutHandler}
+                      >
+                        Confirm Sign Out
+                      </AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              </DropdownMenuItem>
+            </ShineBorder>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </header>
   );
