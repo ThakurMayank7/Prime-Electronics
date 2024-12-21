7b9bfd4ad715953932c146ef40b8d1b9d5c56ed0
Mayank <mayanksingh8181335335@gmail.com>
2024-12-21 19:57:35 +0530

    made some changes

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,38 +1,25 @@
-'use client';
+"use client";
 
-import { useAuth } from '@/hooks/useAuth';
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react'
+import { useAuth } from "@/hooks/useAuth";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
 function Category() {
+  const { user, loading } = useAuth();
 
+  const router = useRouter();
 
-    const { user, loading } = useAuth();
-
-    const router = useRouter();
-  
-    useEffect(() => {
-      if (user === null && loading === false) {
-        router.push("/login");
-      }
-    }, [user, router, loading]);
-  
-    if (loading) {
-      return <p>loading...</p>;
+  useEffect(() => {
+    if (user === null && loading === false) {
+      router.push("/login");
     }
+  }, [user, router, loading]);
 
+  if (loading) {
+    return <p>loading...</p>;
+  }
 
-
-
-
-
-
-
-
-
-  return (
-    <div>Category</div>
-  )
+  return <div>Category</div>;
 }
 
-export default Category
+export default Category;
