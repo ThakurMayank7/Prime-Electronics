ffb5087d4489daae43d07eb7060756b43cff2c39
Mayank <mayanksingh8181335335@gmail.com>
2024-12-22 09:20:20 +0530

    created search component

--- components/Header.tsx
+++ components/Header.tsx
@@ -30,6 +30,7 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import SearchBar from "./SearchBar";
 
 function Header() {
   // const [opened,setOpened]=useState();
@@ -54,37 +55,13 @@ function Header() {
       </span>
 
       <div className="text-white mx-auto bg-pallette2 p-1  rounded border-2 border-pallette3 ">
-        <div className="flex flex-row items-center bg-IcyBlue text-black rounded p-1">
-          <span className="bg-white  rounded p-1 hover:bg-pallette6 hover:text-white hover cursor-pointer border-2 border-black">
-            Search
-          </span>
-
-          <Separator
-            orientation="vertical"
-            className="bg-black h-8 ml-1 w-[1px] "
-          />
-
-          <div className=" ">
-            <input
-              className="rounded mx-1 text-black p-1 border-2 border-DarkNeutral"
-              type="text"
-              placeholder='Try : "Kettles"'
-            />
-          </div>
-          <Separator orientation="vertical" className="bg-black h-8 mr-1" />
-
-          <div className="bg-white p-1 rounded border-2 border-black hover:cursor-pointer hover:bg-gray-200">
-            <Filter className="" />
-          </div>
-        </div>
+        <SearchBar />
       </div>
 
       <div
         className={`ml-auto text-white flex gap-1 mr-2 p-1 bg-pallette3 rounded-full hover:border-2`}
       >
-        <DropdownMenu
-        // onOpenChange={()=>console.log("dropdown")}
-        >
+        <DropdownMenu>
           <DropdownMenuTrigger>
             <Avatar className="rounded-full">
               <AvatarImage
