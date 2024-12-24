22e6de56a133d98d0a2a0e905f735932fff0d25d
Mayank <mayanksingh8181335335@gmail.com>
2024-12-24 13:04:11 +0530

    made some updates

--- app/items/[item]/page.tsx
+++ app/items/[item]/page.tsx
@@ -8,16 +8,59 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/hooks/useAuth";
 import Spinner from "@/components/BlocksSpinner";
+import { db } from "@/firebase/firebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
+
+interface ItemDetails {
+  id: string;
+  displayName: string;
+  description: string;
+  displayImage: string;
+  price: number;
+  features: string[];
+  images: string[];
+  brandId: string;
+  category: string[];
+}
 
 function ItemPage() {
   const pathname = usePathname();
 
-  // Extract the itemId from the pathname (e.g., /items/[item])
   const itemId = pathname?.split("/")[2]; // Assuming the path is /items/[item]
 
   const { user, loading } = useAuth();
 
   const router = useRouter();
+
+  const [itemDetails, setItemDetails] = React.useState<ItemDetails | null>(
+    null
+  );
+
+  useEffect(() => {
+    try {
+      const fetchItemDetails = async () => {
+        const itemSnapshot = await getDoc(doc(db, "items", itemId));
+
+        if (itemSnapshot.exists()) {
+          const itemData = itemSnapshot.data();
+          setItemDetails({
+            id: itemSnapshot.id,
+            displayName: itemData.itemName,
+            description: itemData.itemDescription,
+            displayImage: itemData.displayImageRef,
+            price: itemData.price,
+            features: itemData.features,
+            images: itemData.imagesRefs,
+            brandId: itemData.brandId,
+            category: itemData.category,
+          });
+        }
+      };
+      fetchItemDetails();
+    } catch (e) {
+      console.log(e);
+    }
+  }, [itemId]);
 
   useEffect(() => {
     if (user === null && loading === false) {
@@ -25,7 +68,7 @@ function ItemPage() {
     }
   }, [user, router, loading]);
 
-  if (loading) {
+  if (loading || itemDetails === null) {
     return (
       <div className="h-screen w-screen items-center flex justify-center">
         <Spinner />
@@ -33,39 +76,25 @@ function ItemPage() {
     );
   }
 
-  const item = {
-    title: "AlienWare AB24",
-    description: "The best gaming laptop for 2024 with the latest technology!",
-    price: 1599.99,
-    featuredImage: "https://www.example.com/alienware-ab24.jpg",
-    images: [
-      "https://www.example.com/alienware-ab24-1.jpg",
-      "https://www.example.com/alienware-ab24-2.jpg",
-      "https://www.example.com/alienware-ab24-3.jpg",
-    ],
-    features: ["16GB RAM", "NVIDIA RTX 3080", "1TB SSD", "144Hz display"],
-  };
-
   return (
     <div>
-      {itemId}
       <div className="max-w-7xl mx-auto py-12 px-6">
         {/* Item Details Section */}
         <div className="flex flex-col lg:flex-row">
           {/* Left Column: Item Image Gallery */}
           <div className="lg:w-1/2 flex flex-col items-center space-y-4">
             <CldImage
-              src={item.featuredImage}
+              src={itemDetails.displayImage || "samples/balloons"}
               width="500"
               height="500"
-              alt={item.title}
+              alt={itemDetails.displayName}
               className="rounded-lg shadow-lg object-cover"
             />
             <div className="flex space-x-4">
-              {item.images.map((image, index) => (
+              {itemDetails.images.map((image, index) => (
                 <CldImage
                   key={index}
-                  src={image}
+                  src={image || "samples/balloons"}
                   width="100"
                   height="100"
                   alt={`Additional image ${index + 1}`}
@@ -78,20 +107,25 @@ function ItemPage() {
           {/* Right Column: Item Info */}
           <div className="lg:w-1/2 lg:pl-10 mt-8 lg:mt-0">
             <h1 className="text-3xl font-semibold text-gray-800">
-              {item.title}
+              {itemDetails.displayName}
             </h1>
-            <p className="text-xl text-gray-600 mt-2">${item.price}</p>
-            <p className="text-lg text-gray-700 mt-4">{item.description}</p>
+            <p className="text-xl text-gray-600 mt-2">
+              ₹{Intl.NumberFormat("en-IN").format(itemDetails.price)}
+            </p>
+            <p className="text-lg text-gray-700 mt-4">
+              {itemDetails.description}
+            </p>
 
             {/* Features List */}
             <div className="mt-6">
               <h3 className="text-2xl font-medium text-gray-800">Features:</h3>
               <ul className="list-disc pl-6 text-gray-600 mt-2">
-                {item.features.map((feature, index) => (
-                  <li key={index} className="mt-1">
-                    {feature}
-                  </li>
-                ))}
+                {itemDetails &&
+                  itemDetails?.features?.map((feature, index) => (
+                    <li key={index} className="mt-1">
+                      {feature}
+                    </li>
+                  ))}
               </ul>
             </div>
 
