a1df4c52c489bf9ee342a79acf6856bf9515c26f
Mayank <mayanksingh8181335335@gmail.com>
2024-12-27 19:49:23 +0530

    made some changes for build

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,29 +1,25 @@
-import { useAuth } from '@/hooks/useAuth';
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react'
+"use client";
+
+import { useAuth } from "@/hooks/useAuth";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
 function About() {
+  const { user, loading } = useAuth();
 
-    const { user, loading } = useAuth();
+  const router = useRouter();
 
-    const router = useRouter();
-  
-    useEffect(() => {
-      if (user === null && loading === false) {
-        router.push("/login");
-      }
-    }, [user, router, loading]);
-  
-    if (loading) {
-      return <p>loading...</p>;
+  useEffect(() => {
+    if (user === null && loading === false) {
+      router.push("/login");
     }
+  }, [user, router, loading]);
 
+  if (loading) {
+    return <p>loading...</p>;
+  }
 
-
-
-  return (
-    <div>About</div>
-  )
+  return <div>About</div>;
 }
 
-export default About
+export default About;
