b8f67808a97f9ea39feb31930343e561aa34e670
Mayank <mayanksingh8181335335@gmail.com>
2024-12-27 20:14:38 +0530

    changes for deployment

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useAuth } from "@/hooks/useAuth";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 
 function About() {
